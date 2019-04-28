c5e89a95c190d45b881c26bb46b66651158c192b
Ramanath Madiwal <ramanathrm.kumta@gmail.com>
2019-04-28 21:46:58 +0530

    server set up done

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+app.use(bodyparser.json());
+const PORT = process.env.PORT || 3001;
+
+const appPath = path.join(__dirname, '..', 'dist');
+
+app.use(express.static(appPath));
+console.log('The code reached  main server');
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(appPath, 'index.html'));
+});
+
+app.listen(PORT, function() {
+  console.log('App is running at Port !' + PORT);
+});
